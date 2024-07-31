69a30b02a9cb2857f9799e00db728d2a1455875b
uxjp <joaopedro1172.com>
2024-07-31 17:43:48 -0300

    feat(java): add initial setup for Java development environment
    Copying the files from the Go repo, and using as base. Files are unchanged because I want to observe the diff.
    - Added Dockerfile for creating Golang development environment
    - Added README.md with instructions and project details
    - Included auth-doppler.sh for managing authentication
    - Included build-image.sh for building Docker images
    - Added dev.sh for starting the development environment
    - Added run_container.sh for running the container
    - Included images/dev-sh.png as a visual aid

---
+++ java/run_container.sh
@@ -0,0 +1,25 @@
+docker run -it --name go-ready-env go-ready-env bash -c "
+
+  # Create the .ssh directory if it doesn't exist
+  mkdir -p ~/.ssh
+
+  # Add the provided public SSH keys to files
+  echo \"$ID_RSA_PUB_GITHUB\" > ~/.ssh/id_rsa.pub
+  echo \"$ID_RSA_GITHUB\" > ~/.ssh/id_rsa
+
+  # Set the appropriate permissions for the SSH key files
+  chmod 644 ~/.ssh/id_rsa.pub
+  chmod 600 ~/.ssh/id_rsa
+
+  # Add GitHub's SSH keys to the known_hosts file
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+  # Configure Git with the provided username and email
+  git config --global user.name \"$GITHUB_NAME\"
+  git config --global user.email \"$GITHUB_EMAIL\"
+
+  # Start an interactive bash shell
+  echo 'Hello, your container is ready.'
+
+  bash
+"
